78e5a7ffb0c6ccf7119866a5e836858ef6463b0c
Jun Shern Lim <junshern@Juns-MacBook-Air.local>
2022-02-27 05:23:03 -0500

    formatting changes

--- example/get_statement_data.py
+++ example/get_statement_data.py
@@ -7,7 +7,7 @@ xbrlApi = XbrlApi("7d84412d1c2236517aba57d5aab13020072b7eb116b10f3e0855189cd840e
 
 def getStatementData(userInput):
     user_input = userInput
-
+    MIL = 10 ** 6
     try:
         f = open("./Annual_Reports/" + str(user_input).casefold() + "_report.json", "r")
         annual_report = json.load(f)
@@ -53,40 +53,40 @@ def getStatementData(userInput):
     if "RevenueFromContractWithCustomerExcludingAssessedTax" in income_statement:
         for revenue in income_statement["RevenueFromContractWithCustomerExcludingAssessedTax"]:
             if "segment" not in revenue:
-                simple_data["Revenue"] = round(float(revenue["value"]), 2)
+                simple_data["Revenue ($)"] = float(revenue["value"]) / MIL
                 break
 
     if "Revenues" in income_statement:
-        simple_data["Revenue"] = round(float(income_statement["Revenues"][0]["value"]),2)
+        simple_data["Revenue ($)"] = float(income_statement["Revenues"][0]["value"]) / MIL
 
     if "Revenue" not in simple_data:
-        simple_data["Revenue"] = None
+        simple_data["Revenue ($)"] = None
 
 
     #summing other sources of income
     if "OtherIncome" in income_statement:
-        simple_data["Other Income"] = round(float(income_statement["OtherIncome"][0]["value"]),2)
+        simple_data["Other Income ($)"] = float(income_statement["OtherIncome"][0]["value"]) / MIL
     else:
-        simple_data["Other Income"] = 0
+        simple_data["Other Income ($)"] = 0
 
     if "NonoperatingIncomeExpense" in income_statement:
-        simple_data["Other Income"] += round(float(income_statement["NonoperatingIncomeExpense"][0]["value"]),2)
+        simple_data["Other Income ($)"] += float(income_statement["NonoperatingIncomeExpense"][0]["value"]) / MIL
 
     if "OtherNonoperatingIncomeExpense" in income_statement:
-        simple_data["Other Income"] += float(income_statement["OtherNonoperatingIncomeExpense"][0]["value"])
+        simple_data["Other Income ($)"] += float(income_statement["OtherNonoperatingIncomeExpense"][0]["value"]) / MIL
 
     if "InvestmentIncomeInterest" in income_statement:
-        simple_data["Other Income"] += float(income_statement["InvestmentIncomeInterest"][0]["value"])
+        simple_data["Other Income ($)"] += float(income_statement["InvestmentIncomeInterest"][0]["value"]) / MIL
 
 
 
     #retrieving gross profit from income statement
     if "GrossProfit" in income_statement:
-        simple_data["Gross Profit"] =  float(income_statement["GrossProfit"][0]["value"])
+        simple_data["Gross Profit ($)"] =  float(income_statement["GrossProfit"][0]["value"]) / MIL
     else:
         for key in income_statement:
             if "CostOf" in key:
-                simple_data["Gross Profit"] = simple_data["Revenue"] - float(income_statement[key][0]["value"])
+                simple_data["Gross Profit ($)"] = (simple_data["Revenue"] - float(income_statement[key][0]["value"])) / MIL
 
     #calculating gross profit margin
     try:
@@ -98,16 +98,16 @@ def getStatementData(userInput):
 
     #retrieving net income from income statement
     if "NetIncomeLoss" in income_statement:
-        simple_data["Net Income"] = float(income_statement["NetIncomeLoss"][0]["value"])
+        simple_data["Net Income ($)"] = float(income_statement["NetIncomeLoss"][0]["value"]) / MIL
     else:
-        simple_data["Net Income"] = None
+        simple_data["Net Income ($)"] = None
 
 
     #calculating net profit margin
     try:
         simple_data["Net Profit Margin (%)"] = round(simple_data["Net Income"] / (simple_data["Revenue"] + simple_data["Other Income"]) * 100, 2)
     except:
-        simple_data["Net Profit Margin (%) "] = None
+        simple_data["Net Profit Margin (%)"] = None
 
     #retrieving diluted earnings per share
     if "EarningsPerShareDiluted" in income_statement:
@@ -134,34 +134,34 @@ def getStatementData(userInput):
     balance_sheet = annual_report["BalanceSheets"]
 
     if "AssetsCurrent" in balance_sheet:
-        simple_data["Current Assets"] = float(balance_sheet["AssetsCurrent"][0]["value"])
+        simple_data["Current Assets ($)"] = float(balance_sheet["AssetsCurrent"][0]["value"]) / MIL
     else:
-        simple_data["Current Assets"] = None
+        simple_data["Current Assets ($)"] = None
 
     if "Assets" in balance_sheet:
-        simple_data["Total Assets"] = float(balance_sheet["Assets"][0]["value"])
+        simple_data["Total Assets ($)"] = float(balance_sheet["Assets"][0]["value"]) / MIL
     else:
-        simple_data["Total Assets"] = None
+        simple_data["Total Assets ($)"] = None
 
     if "LiabilitiesCurrent" in balance_sheet:
-        simple_data["Current Liabilities"] = float(balance_sheet["LiabilitiesCurrent"][0]["value"])
+        simple_data["Current Liabilities ($)"] = float(balance_sheet["LiabilitiesCurrent"][0]["value"]) / MIL
     else:
-        simple_data["Current Liabilities"] = None
+        simple_data["Current Liabilities ($)"] = None
 
     if "Liabilities" in balance_sheet:
-        simple_data["Total Liablities"] = float(balance_sheet["Liabilities"][0]["value"])
+        simple_data["Total Liablities ($)"] = float(balance_sheet["Liabilities"][0]["value"]) / MIL
     else:
-        simple_data["Total Liablities"] = None
+        simple_data["Total Liablities ($)"] = None
 
     if "StockholdersEquity" in balance_sheet:
-        simple_data["Total Stockholders Equity"] =  float(balance_sheet["StockholdersEquity"][0]["value"])
+        simple_data["Total Stockholders Equity ($)"] =  float(balance_sheet["StockholdersEquity"][0]["value"]) / MIL
     else:
-        simple_data["Total Stockholders Equity"] = None
+        simple_data["Total Stockholders Equity ($)"] = None
 
     if "LiabilitiesAndStockholdersEquity" in balance_sheet:
-        simple_data["Total Liabilities and Stockholders Equity"] =  float(balance_sheet["LiabilitiesAndStockholdersEquity"][0]["value"])
+        simple_data["Total Liabilities and Stockholders Equity ($)"] =  float(balance_sheet["LiabilitiesAndStockholdersEquity"][0]["value"]) / MIL
     else:
-        simple_data["Total Liabilities and Stockholders Equity"] = None
+        simple_data["Total Liabilities and Stockholders Equity ($)"] = None
 
 
 
